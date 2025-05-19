6fa8d28149c970b945c9b1ace27fdb0543f6f88b
pottsdj <pottsdj@miamioh.edu>
2025-05-18 20:49:13 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -7,16 +7,15 @@ def connect_db():
     """Connect to the SQLite database."""
     return sqlite3.connect(DATABASE)
 
-def generate_test_data(num_items):
-    """Generate test data for the items table."""
+def generate_test_data(num_contacts):
+    """Generate test data for the contacts table."""
     db = connect_db()
-    for i in range(num_items):
+    for i in range(num_contacts):
         name = f'Test Name {i}'
-        item_id = f'123456789{i}'
-        price = f'12{i}'
-        db.execute('INSERT INTO items (name, item_id, price) VALUES (?, ?, ?)', (name, item_id, price))
+        phone = f'123-456-789{i}'
+        db.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
     db.commit()
-    print(f'{num_items} test items added to the database.')
+    print(f'{num_contacts} test contacts added to the database.')
     db.close()
 
 if __name__ == '__main__':
